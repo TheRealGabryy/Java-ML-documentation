0c336f2d2e7e98be3deff633d27244e55b54fb70
Gabriele B <the.real.gabryy@gmail.com>
2025-12-24 12:23:46 +0100

    Init Commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Single:wght@100..900&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&family=Zalando+Sans+SemiExpanded:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>LEARN ML IN JAVA</title>
+</head>
+<body>
+<header class="header">
+    <p class="header-title">LEARN HOW TO CODE ML</p>
+    <p class="header-subtitle">enough theory, learn how to actually implement algorithms</p>
+</header>
+
+<main class="main">
+    <section class="about-section">
+        <p class="title"> ABOUT THE WEBSITE </p>
+        <p class="paragraph">
+            The purpose of this website is to learn ML from scratch. focusing on learning the CODE. first, we'll do <br>
+            algorithms form 0 in JAVA, then, at the end of this series, there will be examples on how to code in python.<br>
+            This website has been build on top of the notes that me, <a
+                    href="https://www.youtube.com/@gabriele-bolognese" target=”_blank” >Gabriele Bolognese</a> <br>
+            built in while studying ML. i built this website because:
+        <ul class="un-list">
+            <li> there are NO coding tutorials on youtube that start from zero</li>
+            <li> there are NO java tutorials. all tutorials use python and they use premade libraries <br> but that's not
+                learning ML. thats' learning the libraries themselves.
+            </li>
+            <li> websites that teach how to code ai are rare and uncomplete. they either start NOT from the start, or
+                they don't finish
+            </li>
+        </ul>
+        <br>
+        <p class="paragraph">This website is a simple, open source, easy to follow website to start your ML journey from 0, and start coding
+        with it. <br>
+        this website is divided in 2 sections, 1 is <b>ML theory</b> one is <b>ML practice </b>. while it's not <br>
+        necessary to learn the theory immediately i would strongly recommend to.
+        </p>
+    </section>
+
+    <section class="prerequisites-section">
+        <p class="title">PREREQUITES</p>
+        <p class="big-paragraph">
+            This course needs you to know the <b>Basics of java programming</b>, so know what are:
+
+        <ul class="un-list">
+            <li>For/while loops / data types, all the basic syntax of java</li>
+            <li>Classes and basics of OOP</li>
+            <li>Know math</li>
+            <li>know how to problem solve with code</li>
+        </ul>
+        </p>
+    </section>
+
+    <section class="start-section">
+        <p class="title"> START THE JOURNEY!</p>
+        <p class="paragraph">
+            start your ML journey here. press one of the 2 buttons to go in the theory or code sections.<br>
+            note that: the website is always updated and some paragraphs <b> may change </b>. <br>
+            please refer to the section <b>below</b> for updates on this website. <br>
+            If you have any <b>suggestions, improvement, or you want to flag an error</b>, please use the <a href="https://discord.gg/UbJP7taetY">discord</a> server <br>
+        </p>
+
+        <a href="Pages/Code/start.php"><button class="coding-button">ML coding</button></a>
+        <a href="Pages/Code/start.php"><button class="theory-button">ML theory</button></a>
+        <br> <br> <br> <br>.
+    </section>
+
+    <section class="updates-section">
+        <p class="title">UPDATES</p>
+        <p class="update">-- <b> 24/12/2025</b> Started this website --</p>
+    </section>
+</main>
+</body>
+</html>
